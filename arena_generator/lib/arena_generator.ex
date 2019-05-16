7d975557949d1785cd318991730f4be608b5f03a
Vincent Leclair <vinleclair@gmail.com>
2019-05-16 14:37:39 -0400

    Add empty arena generator

--- arena_generator/lib/arena_generator.ex
+++ arena_generator/lib/arena_generator.ex
@@ -1,25 +1,31 @@
 defmodule ArenaGenerator do
+  @rocks_spawn_chance 30
+  @rock_count 5 #TODO Turn this into a flag
+
   @moduledoc """
   Simple arena generator for D&D.
   """
 
   @doc """
-
-
-  ## Examples
-
-  #iex> ArenaGenerator.generate(4, 4)
-      "[
-        0 0 0 0
-        0 0 0 0
-        0 0 0 0
-        0 0 0 0 
-      ]"
-
+  Generate an empty arena based on width and height input
   """
-  def generate(width, height) do
-    grid = Enum.reduce(1..height, [], fn x, h -> 
-      h ++ [Enum.reduce(1..width, [], fn y, w -> 
-        w ++ if Enum.random(1..10) == 1, do: ["X"], else: ["O"] end)] end)
+  @spec generate_empty_arena(integer, integer) :: List
+  def generate_empty_arena(width, height) do
+    Enum.reduce(0..width - 1, %{}, fn x, x_acc -> 
+      Map.put(x_acc, x, Enum.reduce(0..height - 1, %{}, fn y, y_acc -> 
+        Map.put(y_acc, y, "O") 
+      end)) 
+    end)
   end
+
+  @doc """
+  Add rock formations to an existing arena
+  """
+  @spec add_rocks(List) :: List
+  def add_rocks(arena) do
+    #TODO implement
+  end
+
+  defp do_add_rocks(arena, rock_count) when rock_count == 0, do: arena
+
 end
--- arena_generator/lib/commandline/cli.ex
+++ arena_generator/lib/commandline/cli.ex
@@ -27,7 +27,8 @@ defmodule ArenaGenerator.CLI do
   defp execute_command([size: size]) do
     if valid_size?(size) do
       [width, height] = String.split(size, ~r/[xX]/, trim: true)
-      ArenaGenerator.generate(String.to_integer(width), String.to_integer(height))
+      arena = ArenaGenerator.generate(String.to_integer(width), String.to_integer(height))
+      for row <- arena, do: IO.puts List.to_string(row)
     else
       IO.puts Constants.CLI.invalid_size
     end
