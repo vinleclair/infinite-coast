ac8bb57bec601b6c4c57e4ec3baeb04827611dae
Vincent Leclair <vinleclair@gmail.com>
2019-06-03 15:37:42 -0400

    Add scenario generator test

--- test/merchant_generator_test.exs
+++ test/merchant_generator_test.exs
@@ -5,8 +5,8 @@ defmodule MerchantGeneratorTest do
   test "generate a random merchant table then delete it" do
     level = Enum.random(1..20)
     MerchantGenerator.generate_merchant_table(level)
-    assert File.exists?("config/merchant_table_level_#{level}.yaml")
-    File.rm("config/merchant_table_level_#{level}.yaml")
-    refute File.exists?("config/merchant_table_level_#{level}.yaml")
+    assert File.exists?("./merchant_table_level_#{level}.yaml")
+    File.rm("./merchant_table_level_#{level}.yaml")
+    refute File.exists?("./merchant_table_level_#{level}.yaml")
   end
 end
